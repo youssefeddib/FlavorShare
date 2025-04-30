96036973cf86df4f60edfa0d0ba8f6b6dc0dfbd3
youssef eddib <eddiby79@gmail.com>
2025-04-30 17:06:43 +0200

    creation navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RecipeListComponent } from './components/recipe-list/recipe-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from "./components/home/home.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [CommonModule, HttpClientModule, RouterOutlet, FooterComponent, NavbarComponent, RecipeListComponent, HomeComponent], // if needed
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'FlavorShare';
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
 }
